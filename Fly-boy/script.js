d7c1f16d49c0c5cf57b85d000ac8e9d772746fb0
Hixmixly <152249445+Hixmixly@users.noreply.github.com>
2024-02-20 20:56:19 -0800

    didnt work

--- Fly-boy/script.js
+++ Fly-boy/script.js
@@ -1,7 +1,23 @@
 // using this as a ref -https://www.w3schools.com/graphics/game_controllers.asp
 
 const hero = document.getElementById('heroShip')
-const canvas = document.getElementById('canvas')
+const gameArea = document.getElementById('canvas')
+
+const hero{
+window.addEventListener ('keydown', function (e){
+    gameArea.key = e.keyCode;
+
+})
+
+window.addEventListener ('keyup', function (e) {
+    gameArea.key = false;
+})
 
 
-
+myGamePiece.speedX = 0;
+myGamePiece.speedY = 0;
+if (myGameArea.key && myGameArea.key == 37) {myGamePiece.speedX = -1; }
+if (myGameArea.key && myGameArea.key == 39) {myGamePiece.speedX = 1; }
+if (myGameArea.key && myGameArea.key == 38) {myGamePiece.speedY = -1; }
+if (myGameArea.key && myGameArea.key == 40) {myGamePiece.speedY = 1; }
+}
