44e1c3a595ee684ba0dafc94f7096a17dc945fea
Hixmixly <152249445+Hixmixly@users.noreply.github.com>
2024-02-20 20:38:33 -0800

    before char movement

---
+++ Fly-boy/script.js
@@ -0,0 +1,7 @@
+// using this as a ref -https://www.w3schools.com/graphics/game_controllers.asp
+
+const hero = document.getElementById('heroShip')
+const canvas = document.getElementById('canvas')
+
+
+
