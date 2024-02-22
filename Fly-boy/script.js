8950d5705c9aacafb2a9279eff3bd75966bb144c
Hixmixly <152249445+Hixmixly@users.noreply.github.com>
2024-02-21 22:24:38 -0800

    moving obsticle

--- Fly-boy/script.js
+++ Fly-boy/script.js
@@ -1,23 +1,10 @@
 // using this as a ref -https://www.w3schools.com/graphics/game_controllers.asp
 
-const hero = document.getElementById('heroShip')
+
+
 const gameArea = document.getElementById('canvas')
 
-const hero{
-window.addEventListener ('keydown', function (e){
-    gameArea.key = e.keyCode;
-
-})
-
-window.addEventListener ('keyup', function (e) {
-    gameArea.key = false;
-})
+const hero = document.getElementById('heroShip') 
+    move (hero).arrowKeys(x, y)
 
 
-myGamePiece.speedX = 0;
-myGamePiece.speedY = 0;
-if (myGameArea.key && myGameArea.key == 37) {myGamePiece.speedX = -1; }
-if (myGameArea.key && myGameArea.key == 39) {myGamePiece.speedX = 1; }
-if (myGameArea.key && myGameArea.key == 38) {myGamePiece.speedY = -1; }
-if (myGameArea.key && myGameArea.key == 40) {myGamePiece.speedY = 1; }
-}
