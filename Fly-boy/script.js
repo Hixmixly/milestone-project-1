efb833d6a0bc0e326cd2e189a4a47739653a44d7
Hixmixly <152249445+Hixmixly@users.noreply.github.com>
2024-02-22 19:07:42 -0800

    obsticle fixed

--- Fly-boy/script.js
+++ Fly-boy/script.js
@@ -1,9 +1,10 @@
 // using this as a ref -https://www.w3schools.com/graphics/game_controllers.asp
 
 var block = document.getElementById("obsticle");
-var hole = document.getElementById("hole")
+var hole = document.getElementById("hole");
 
 hole.addEventListener ("animationiteration", () => {
-    var random = (Math.random() * 300)
+    var random = -((Math.random() * 450) + 150);
+    hole.style.top = random + "px";
 
 });
