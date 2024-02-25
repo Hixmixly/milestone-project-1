c4ec3a16f05809bad94101c52893c8207aef6cc4
Hixmixly <152249445+Hixmixly@users.noreply.github.com>
2024-02-25 14:05:23 -0800

    gravity worked, now it does not

--- Fly-boy/script.js
+++ Fly-boy/script.js
@@ -1,11 +1,10 @@
 // using this as a ref -https://www.w3schools.com/graphics/game_controllers.asp
 
+let arena = document.getElementById("canvas");
 let block = document.getElementById("obsticle");
 let hole = document.getElementById("hole");
 const player = document.getElementById("heroShip");
-let moveby = 10;
-
-
+let flying = 0;
 
 
 hole.addEventListener ("animationiteration", () => {
@@ -14,35 +13,60 @@ hole.addEventListener ("animationiteration", () => {
 
 });
 
-/*setInterval (function(){
-    let pcTop = 
-    parseInt(window.getComputedStyle(pc).getPropertyValue("top"));
-    pc.style.top = (pcTop + 3) + "px"
-},10);*/
+setInterval (function(){
+    let playerTop = 
+    parseInt(window.getComputedStyle(player).getPropertyValue("top"));
+   if (jumpling == 0) {
+      player.style.top = (playerTop + 3) + "px";
+   }
+  
+}, 10);
+
+function fly (){
+    flying = 1;
+    let flyCount = 0;
+    let flyInterval = setInterval(function(){
+        let playerTop =
+        parseInt(window.getComputedStyle(player).getPropertyValue("top"));
+       if((playerTop > 5)&&(counter<15)){
+            player.style.top = (playerTop -6) + "px";
+        }
+       
+        if(flyCount > 15){
+            clearInterval(flyInterval);
+            flying=0;
+            flyCount=0;
+        }
+    flyCount ++;
+    }, 10);
+}
 
 
-window.addEventListener('load', ()=> {
-    player.style.left = 0;
-    player.style.top = 0;
 
-});
+addEventListener ('keydown', ({keyCode}) => {
+        switch (keyCode) {
+            case 37:
+                console.log('left')
+                break
 
-window.addEventListener('keyup', (e)=> {
-    switch (e.key) {
-        case 'ArrowLeft':
-            player.style.left = parseInt(player.style.left) - moveby + 'px';
-            break; 
-        case 'ArrowRight':
-            player.style.left = parseInt(player.style.left) + moveby + 'px';
-            break; 
-        case 'ArrowUp':
-            player.style.top = parseInt(player.style.top) - moveby + 'px';
-            break; 
-        case 'ArrowDown':
-            player.style.top = parseInt(player.style.top) + moveby + 'px';
-            break; 
-    }
-});
+            case 38:
+                console.log('up')
+                break  
+                
+            case 39:
+                 console.log('right')
+                break
+                    
+            case 40:
+                console.log('down')
+                break
+        }
+})
+//38 up, 39 right, 40 down, 37 left
+
+document.addEventListener("keyDown", e => {
+    
+})
 
 
 
